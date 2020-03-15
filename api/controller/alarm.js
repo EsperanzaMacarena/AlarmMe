0a517c8b16c2fa34ca3a4260afec0d5d8d876b5b
JMBargueno <jmbarguenolopez@gmail.com>
2020-03-15 10:40:09 +0100

    Progreso endpoint alarma

---
+++ api/controller/alarm.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+const error_types = require('./error_types');
+const Alarm = require ('../model/alarm');
+
+module.exports = {
+
+    newAlarm:(req,res)=>{
+        let alarm = new Alarm({
+
+        });
+
+    }
+
+}
