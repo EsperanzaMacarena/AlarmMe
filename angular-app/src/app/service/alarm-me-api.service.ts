ca92a4d7b30a8ee8dfb931e0879f1ddd3142cf36
Esperanza Escacena <espemacem@gmail.com>
2020-03-17 18:43:33 +0100

    he actualizado el servicio de alarmmeapi, ya coge el token del localstorage

--- angular-app/src/app/service/alarm-me-api.service.ts
+++ angular-app/src/app/service/alarm-me-api.service.ts
@@ -8,7 +8,7 @@ import {AuthService} from './auth.service';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
-    'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1ZTZiZGM2MDg2YTljOTNjZGNmNDJlZTgiLCJleHAiOjE1ODQ0NjMyMDk2ODYsInVzZXJuYW1lIjoiYWRtaW5AYWRtaW5pc3RyYWRvci5jb20iLCJwYXNzd29yZCI6IjEyMzQ1Njc4In0.uS47pO2wKfppd1RzDyXOWcI65bIThDHJksDYW4TBczw`
+    'Authorization': 'Bearer '+AuthService.getToken()
   })
 };
 @Injectable({
