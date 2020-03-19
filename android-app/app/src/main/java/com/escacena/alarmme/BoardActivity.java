89e41e598dd5441c1713e6e71a7b59a158a63b32
JMBargueno <jmbarguenolopez@gmail.com>
2020-03-19 19:05:23 +0100

    Añadido navigation_profile al AppBarConfiguration

--- android-app/app/src/main/java/com/escacena/alarmme/BoardActivity.java
+++ android-app/app/src/main/java/com/escacena/alarmme/BoardActivity.java
@@ -28,7 +28,7 @@ public class BoardActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_alarms)
+                R.id.navigation_alarms, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
