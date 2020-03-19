12c7cb7cd1889df5b21c51ee46bdb062cbc97b6b
Esperanza Escacena <espemacem@gmail.com>
2020-03-19 14:20:07 +0100

    refactorización de paquete del dashboard para hacer perfil

--- android-app/app/src/main/java/com/escacena/alarmme/ui/profile/ProfileFragment.java
+++ android-app/app/src/main/java/com/escacena/alarmme/ui/profile/ProfileFragment.java
@@ -1,26 +1,22 @@
-package com.escacena.alarmme.ui.dashboard;
+package com.escacena.alarmme.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.escacena.alarmme.R;
 
-public class DashboardFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
         return root;
     }
